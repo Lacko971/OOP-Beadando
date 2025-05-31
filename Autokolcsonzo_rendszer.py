4a0e38577870e3fca8ac0aad3b56a9cdc0280720
Lacko971 <pizzaman1973@gmail.com>
2025-05-31 19:32:16 +0200

    git commit -m "feat: autótípusok megvalósítása polimorf viselkedéssel"

--- Autokolcsonzo_rendszer.py
+++ Autokolcsonzo_rendszer.py
@@ -10,3 +10,19 @@ class Auto(ABC):
     @abstractmethod
     def __str__(self):
         pass
+class Szemelyauto(Auto):
+    def __init__(self, rendszam, tipus, berleti_dij, ulesek_szama):
+        super().__init__(rendszam, tipus, berleti_dij)
+        self.ulesek_szama = ulesek_szama
+
+    def __str__(self):
+        return f"Személyautó: {self.rendszam}, {self.tipus}, {self.berleti_dij} Ft/nap, {self.ulesek_szama} ülés"
+
+class Teherauto(Auto):
+    def __init__(self, rendszam, tipus, berleti_dij, teherbiras):
+        super().__init__(rendszam, tipus, berleti_dij)
+        self.teherbiras = teherbiras
+
+    def __str__(self):
+        return f"Teherautó: {self.rendszam}, {self.tipus}, {self.berleti_dij} Ft/nap, {self.teherbiras} kg"
+    
